84dc1852c27b7509c051b55c3f26da9a33012821
Sean Kelly <invertedrdm@gmail.com>
2021-04-07 16:56:59 -0400

    adding next lair of menu prompts and functions

--- assets/prompts.js
+++ assets/prompts.js
@@ -10,7 +10,49 @@ const startupPrompts = [
             "Exit"
         ]
     }
-]
+];
+
+const addPrompts = [
+    {
+        type: 'list',
+        name: 'task',
+        message: 'What do you want to do?',
+        choices: [
+            "Add departments, roles, and/or employees",
+            "View departments, roles, employees",
+            "Update employee roles",
+            "Go Back"
+        ]
+    }
+];
+
+const viewPrompts = [
+    {
+        type: 'list',
+        name: 'task',
+        message: 'What do you want to do?',
+        choices: [
+            "Add departments, roles, and/or employees",
+            "View departments, roles, employees",
+            "Update employee roles",
+            "Go Back"
+        ]
+    }
+];
+
+const editPrompts = [
+    {
+        type: 'list',
+        name: 'task',
+        message: 'What do you want to do?',
+        choices: [
+            "Add departments, roles, and/or employees",
+            "View departments, roles, employees",
+            "Update employee roles",
+            "Go Back"
+        ]
+    }
+];
 
 module.exports = {
     startupPrompts,
