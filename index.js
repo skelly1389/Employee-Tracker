b9ba7fc6fc1d9fe8a715aab719882c347bc2f35a
Sean Kelly <invertedrdm@gmail.com>
2021-04-07 19:40:58 -0400

    updated menus

--- index.js
+++ index.js
@@ -8,65 +8,95 @@ const connection = mysql.createConnection({
     user: 'root',
     password: '',
     database: 'employeeDB',
-  });
+});
 
-  const start = () => {
+const start = () => {
     inquirer.prompt(prompts.startupPrompts)
-    .then((answers) => {
-        switch(answers.task) {
-            case "Add departments, roles, and/or employees":
-                addSelector();
-                break;
-            case "View departments, roles, employees":
-                viewSelector();
-                break;
-            case "Update employee roles":
-                updateSelector();
-                break;
-            case "Exit":
-                exit();
-                break
-        }
-    });
-  }
+        .then((answers) => {
+            switch (answers.task) {
+                case "Add departments, roles, and/or employees":
+                    addSelector();
+                    break;
+                case "View departments, roles, employees":
+                    viewSelector();
+                    break;
+                case "Update employee roles":
+                    updateSelector();
+                    break;
+                case "Exit":
+                    exit();
+                    break
+            }
+        });
+}
 
-  function addSelector() {
+function addSelector() {
     inquirer.prompt(prompts.addPrompts)
-    .then((answers) => {
-        switch(answers.task) {
-            case "Departments":
-                addDepartment();
-                break;
-            case "Employees":
-                viewEmployee();
-                break;
-            case "Roles":
-                updateRole();
-                break;
-            case "Go Back":
-                start();
-                break
-        }
-    });
-  }
+        .then((answers) => {
+            switch (answers.task) {
+                case "Departments":
+                    addDepartment();
+                    break;
+                case "Employees":
+                    viewEmployee();
+                    break;
+                case "Roles":
+                    updateRole();
+                    break;
+                case "Go Back":
+                    start();
+                    break
+            }
+        });
+}
 
-  function viewSelector() {
-    inquirer.prompt(prompts.viewPrompts)
-    .then((answers) => console.log(answers.task));
-  }
+function viewSelector() {
+    inquirer.prompt(prompts.addPrompts)
+        .then((answers) => {
+            switch (answers.task) {
+                case "Departments":
+                    viewDepartment();
+                    break;
+                case "Employees":
+                    viewEmployee();
+                    break;
+                case "Roles":
+                    viewRole();
+                    break;
+                case "Go Back":
+                    start();
+                    break
+            }
+        });
+}
 
-  function updateSelector() {
-    inquirer.prompt(prompts.updatePrompts)
-    .then((answers) => console.log(answers.task));
-  }
+function updateSelector() {
+    inquirer.prompt(prompts.addPrompts)
+        .then((answers) => {
+            switch (answers.task) {
+                case "Departments":
+                    updateDepartment();
+                    break;
+                case "Employees":
+                    updateEmployee();
+                    break;
+                case "Roles":
+                    updateRole();
+                    break;
+                case "Go Back":
+                    start();
+                    break
+            }
+        });
+}
 
-  function exit() {
-      console.log("later nerd");
-      connection.end();
-  }
+function exit() {
+    console.log("later nerd");
+    connection.end();
+}
 
-  connection.connect((err) => {
+connection.connect((err) => {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}`);
     start();
-  });
+});
